bde3ded02ca71431ff3e8f3359d39840ab4dd7c0
patil1405 <53460809+patil1405@users.noreply.github.com>
2024-02-16 17:23:05 +0530

    Update EmployeeController.java

--- Employee_Management/src/main/java/com/example/Employee_Management/Controller/EmployeeController.java
+++ Employee_Management/src/main/java/com/example/Employee_Management/Controller/EmployeeController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:9091/manager/text/list")
+@CrossOrigin(origins = "http://localhost:4200")
 
 public class EmployeeController {
 
